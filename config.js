0e9fe0c4dc04d21de5a7974d65dddd15befa19d3
Thiendz91999 <90498839+Thiendz91999@users.noreply.github.com>
2021-10-10 20:10:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Gửi baby',
-    introDesc: `Hiệp bede`,
-    
-
-
-    btnIntro: 'click vào đây để tiếp tục',
-    title: 'vui lòng chọn A hoặc B',
-    desc: 'có yêu tôi không,trả lời  ',
-    btnYes: 'B.yêu luôn<33',
-    btnNo: 'A.đéo :3',
-    question:'Bạn yêu ai nhất ?? <3',
-    btnReply: 'Gửi Anh iuu <3',
-    reply: 'Võ Hoàng Phúc.....',
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Phúc Xinh Gái.',
-    btnAccept: 'hahaha <3',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
