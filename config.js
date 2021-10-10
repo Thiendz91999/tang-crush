2a1b190c68a09ff0909fc44a8a78ae9e824a94f1
Thiendz91999 <90498839+Thiendz91999@users.noreply.github.com>
2021-10-10 18:44:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Gửi baby',
-    introDesc: `Phương Thảo cute .!!
+    introDesc: `Dương Thúy Huỳnh
     
     
     `,
     btnIntro: 'click vào đây để tiếp tục',
     title: 'vui lòng chọn A hoặc B',
-    desc: 'Phải chăng em đã yêu tôi từ cái nhìn đầu tiên? ',
+    desc: 'có yêu tôi không,trả lời  ',
     btnYes: 'B.yêu luôn<33',
     btnNo: 'A.đéo :3',
     question:'Bạn yêu ai nhất ?? <3',
     btnReply: 'Gửi Anh iuu <3',
     reply: 'Hồng Trí Thiện.....',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Thiện đẹp trai nhất vũ trụ.',
+    messDesc: 'Thiện đẹp trai.',
     btnAccept: 'hahaha <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
